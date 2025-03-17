fc2c9344882c1eaea2e9db60ecc87821e99256e1
halkszavu <halkszavu@outlook.com>
2025-03-17 10:23:49 +0200

    Added Vigenere cipher from the previous HA

--- Code/Homework-Calculations/Homework02-Calculations/Program.cs
+++ Code/Homework-Calculations/Homework02-Calculations/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Homework02_Calculations
+﻿using System.Text;
+
+
+
+namespace Homework02_Calculations
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			
+		}
+
+		private static string VigenereCipher(string message, string password)
+		{
+			StringBuilder result = new(message.Length);
+
+			for (int i = 0; i < message.Length; i++)
+			{
+				char plainChar = message[i];
+				char keyChar = password[i % password.Length]; // Repeat key if text is longer
+
+				char encryptedChar = (char)((plainChar - 'a' + (keyChar - 'a')) % 26 + 'a');
+				result.Append(encryptedChar);
+			}
+
+			return result.ToString();
 		}
 	}
 }

9403c22ef2bdae48269989a2a9bf05aef88604fc
halkszavu <halkszavu@outlook.com>
2025-03-26 00:11:24 +0200

    End of day

---
+++ Code/Homework-Calculations/Homework02-Calculations/OFB.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace Homework02_Calculations
+{
+	internal class OFB
+	{
+		private readonly int[] key;
+		string IV;
+
+		public OFB(int[] key, string IV)
+		{
+			this.key = key;
+			this.IV = IV;
+		}
+
+		public string Encrypt(string message)
+		{
+			IV = PermutationCipher(IV, key);
+			return XOR(message, IV);
+		}
+
+		private static string PermutationCipher(string message, int[] key)
+		{
+			int keylength = key.Length;
+			StringBuilder result = new(message.Length);
+			int i = 0;
+			while (message.Length > keylength * i)
+			{
+				for (int j = 0; j < keylength; j++)
+				{
+					int pos = key[j] + keylength * i - 1;
+					char c = message.Length > pos ? message[pos] : 'X';
+					result.Append(c);
+				}
+				i++;
+			}
+
+			return result.ToString();
+		}
+
+		private static string XOR(string message, string IV)
+		{
+			StringBuilder result = new(message.Length);
+			for (int i = 0; i < message.Length; i++)
+			{
+				char c = (char)(message[i] ^ IV[i]);
+				result.Append(c);
+			}
+			return result.ToString();
+		}
+	}
+}
--- Code/Homework-Calculations/Homework02-Calculations/Program.cs
+++ Code/Homework-Calculations/Homework02-Calculations/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Metrics;
+using System.Text;
 
 using static System.Console;
 
@@ -7,6 +8,12 @@ namespace Homework02_Calculations
 	internal class Program
 	{
 		static void Main(string[] args)
+		{
+			//Task1();
+			Task3();
+		}
+
+		private static void Task1()
 		{
 			WriteLine("Task 1:");
 			WriteLine("Part 1:");
@@ -27,6 +34,11 @@ namespace Homework02_Calculations
 			WriteLine($"Message: {message} encrypted with password: {password} is:\n{encrypted}");
 		}
 
+		private static void Task3()
+		{
+
+		}
+
 		private static string VigenereCipher(string message, string password)
 		{
 			StringBuilder result = new(message.Length);
@@ -41,6 +53,43 @@ namespace Homework02_Calculations
 			}
 
 			return result.ToString();
+		}
+
+		private static string Convert(char c)
+		{
+			char x = c.ToString().ToUpper()[0];
+			string r = "";
+			r = x switch
+			{
+				'A' => "00000",
+				'B' => "00001",
+				'C' => "00010",
+				'D' => "00011",
+				'E' => "00100",
+				'F' => "00101",
+				'G' => "00110",
+				'H' => "00111",
+				'I' => "01000",
+				'J' => "01001",
+				'K' => "01010",
+				'L' => "01011",
+				'M' => "01100",
+				'N' => "01101",
+				'O' => "01110",
+				'P' => "01111",
+				'Q' => "10000",
+				'R' => "10001",
+				'S' => "10010",
+				'T' => "10011",
+				'U' => "10100",
+				'V' => "10101",
+				'W' => "10110",
+				'X' => "10111",
+				'Y' => "11000",
+				'Z' => "11001",
+				_ => "11111",
+			};
+			return r;
 		}
 	}
 }
